89f9b461ca2fae2f960b51f2bead9c3529830936
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-19 19:23:25 -0300

    feat: add linkRepo

--- src/sections/project-section.tsx
+++ src/sections/project-section.tsx
@@ -3,9 +3,9 @@ import { Separator } from "@/components/ui/separator";
 import ImageLeft from "./projects/imageLeft";
 import ImageRight from "./projects/imageRight";
 
-const ProjectSection = () => {
+const ProjectSection = ({ id }: { id: string }) => {
   return (
-    <section className="mx-auto w-full max-w-[90%] select-none py-10">
+    <section className="mx-auto w-full max-w-[90%] select-none py-10" id={id}>
       <div className="mx-auto w-full">
         <p className="mb-8 select-none text-3xl font-black text-white">
           <span className="text-xl text-lime-500">{`. `}</span>Projetos
@@ -19,7 +19,9 @@ const ProjectSection = () => {
         title="Gamtech"
         subtitle="Elevando sua experiência tecnológica!"
         description="A Gamtech é um e-commerce de itens para computadores desenvolvido com React, Typescript, Next.js, NextUI, ShadCN/UI, Tailwind CSS, Prisma, Supabase, Stripe e Vercel. O projeto conta com uma gama de itens como teclados, mouses, headphones, placas de vídeo, placas mãe e processadores. Venha encontrar o próximo upgrade do seu setup na Gamtech!"
-        link="/project/gamtech"
+        link="https://gamtech.vercel.app/"
+        linkRepo="https://github.com/gui-bus/Gamtech"
+        id="Gamtech"
       />
       <Separator className="my-5" />
       <ImageRight
@@ -27,7 +29,9 @@ const ProjectSection = () => {
         title="Hotefy"
         subtitle="O seu portal para viagens inesquecíveis!"
         description="O Hotefy é uma plataforma de reservas de viagens desenvolvido com React, Typescript, Next.js, NextUI, ShadCN/UI, Tailwind CSS, Prisma, Supabase e Vercel. O projeto conta com opçoes de viagens para todos os gostos, venha encontrar o refúgio perfeito e realizar os seus sonhos!"
-        link="/project/hotefy"
+        link="https://hotefy.vercel.app/"
+        linkRepo="https://github.com/gui-bus/Hotefy"
+        id="Hotefy"
       />
       <Separator className="my-5" />
       <ImageLeft
@@ -35,7 +39,9 @@ const ProjectSection = () => {
         title="DriveX"
         subtitle="Experiência automotiva em um novo patamar!"
         description="A DriveX é uma página web de compra e venda de veículos desenvolvido com Vite, React, TypeScript, Firebase e Tailwind CSS. O projeto conta com um sistema de autenticação com Firebase para gerenciar o login e registro de usuários, bem como um banco de dados Firestore para armazenar informações sobre veículos e usuários. Além disso, o projeto oferece a possibilidade de entrar em contato com os vendedores via WhatsApp."
-        link="/project/drivex"
+        link="https://drivex-cars.vercel.app/"
+        linkRepo="https://github.com/gui-bus/DriveX"
+        id="DriveX"
       />
       <Separator className="my-5" />
       <ImageRight
@@ -43,7 +49,9 @@ const ProjectSection = () => {
         title="Cinedex"
         subtitle="Seu catálogo definitivo de filmes e series!"
         description="A Cinedex é uma plataforma online de catálogo de filmes, desenvolvido com React, Typescript, Next.js, NextUI, Tailwind CSS e Vercel. O projeto conta com uma ampla gama de opções de filmes separados por categorias e gêneros para explorar!"
-        link="/project/cinedex"
+        link="https://cinedex.vercel.app/discover/now_playing"
+        linkRepo="https://github.com/gui-bus/Cinedex"
+        id="Cinedex"
       />
       <Separator className="my-5" />
       <ImageLeft
@@ -51,7 +59,9 @@ const ProjectSection = () => {
         title="O Pedal Café"
         subtitle="Gerenciamento eficiente para o seu negócio!"
         description="O projeto do Pedal Café é um sistema completo com Frontend, Backend e Mobile de administração de pedidos de uma cafeteria. O projeto conta com diversas funcionalidades para facilitar a administração e execução dos pedidos, proporcionando um gerenciamento de produtos e pedidos de uma forma poderosa e eficiente!"
-        link="/project/o-pedal-cafe"
+        link="https://o-pedal-cafe.vercel.app/"
+        linkRepo="https://github.com/gui-bus/oPedalCafe"
+        id="oPedalCafe"
       />
        <Separator className="my-5" />
       <ImageRight
@@ -59,7 +69,9 @@ const ProjectSection = () => {
         title="Geoway"
         subtitle="Mapeando aventuras!"
         description="A Geoway é uma Landing Page para um app fictício de rotas para trilhas e viagens desenvolvido com React, Typescript, Next.js, ShadCN/UI, Tailwind CSS e Vercel. O projeto conta com diversas informações sobre o aplicativo, como seu funcionamento e funcionalidades."
-        link="/project/geoway"
+        link="https://geoway.vercel.app/"
+        linkRepo="https://github.com/gui-bus/Geoway"
+        id="Geoway"
       />
     </section>
   );
