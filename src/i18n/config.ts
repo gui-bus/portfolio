512c4216dfbfaa3837179cafddb72cfa96164869
Guilherme Bustamante <guilherme_bustamante@live.com>
2026-04-01 12:44:02 -0300

    chore: change default locale

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,2 +1,2 @@
-export const locales = ['en', 'pt', 'es', 'de', 'fr'] as const;
-export const defaultLocale = 'en' as const;
+export const locales = ['pt', 'en', 'es', 'de', 'fr'] as const;
+export const defaultLocale = 'pt' as const;
