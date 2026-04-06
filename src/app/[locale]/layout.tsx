0e8b227df751439c9b0fbbf8fb422f690a2f01ff
Guilherme Bustamante <guilherme_bustamante@live.com>
2026-04-06 16:57:12 -0300

    fix: fix build error

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import {NextIntlClientProvider} from 'next-intl';
 import {getLocale, getMessages, getTranslations} from 'next-intl/server';
 import { ThemeProvider } from "@/lib/providers/themeProvider";
-//@ts-expect-error Globals import
 import "./globals.css";
 
 const geistSans = Geist({
